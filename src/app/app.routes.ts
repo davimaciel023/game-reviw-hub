555e529b28c7a3e343d96cd3632d58fc6fe483d6
davimaciel077@gmail.com <davimaciel077@gmail.com>
2025-06-18 11:32:23 -0300

    configurando rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { GameListComponentComponent } from './pages/listar/game-list-component/game-list-component.component';
+import { GameDetailComponentComponent } from './pages/detalhes/game-detail-component/game-detail-component.component';
+import { GameFormComponentComponent } from './pages/form/game-form-component/game-form-component.component';
+import { TopGamesComponentComponent } from './pages/topGames/top-games-component/top-games-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listar',
+    pathMatch: 'full'
+  },
+  {
+    path: 'listar',
+    component: GameListComponentComponent
+  },
+  {
+    path: 'detalhes',
+    component: GameDetailComponentComponent
+  },
+  {
+    path: 'form',
+    component: GameFormComponentComponent
+  },
+  {
+    path: 'topGames',
+    component: TopGamesComponentComponent
+  }
+];
