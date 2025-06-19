a420ef611f66a0de36b051c9908344770f9b6174
davimaciel077@gmail.com <davimaciel077@gmail.com>
2025-06-19 11:58:40 -0300

    configurando service

--- src/app/servicos/game-service.service.ts
+++ src/app/servicos/game-service.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Game } from '../interfaces/gameInterfaces.models';
+import { Ratings } from '../interfaces/gameInterfaces.models';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameServiceService {
 
-  constructor() { }
+  private readonly API_URL = 'http://localhost:3000/games'
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  cadastrarGame(game: Game): Observable<Game> {
+    return this.http.post<Game>(this.API_URL, game)
+  }
+
 }
