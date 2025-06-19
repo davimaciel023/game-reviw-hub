628faf822fc1abc3007de78c24e54ccef1bb3576
davimaciel077@gmail.com <davimaciel077@gmail.com>
2025-06-18 22:15:53 -0300

    configurando pagina de cadastro de game

--- src/app/pages/listar/game-list-component/game-list-component.component.ts
+++ src/app/pages/listar/game-list-component/game-list-component.component.ts
@@ -1,15 +1,19 @@
+import { RatingModule } from 'primeng/rating';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-impor
+import { FormsModule } from '@angular/forms';
 
 
 
 @Component({
   selector: 'app-game-list-component',
-  imports: [CommonModule],
+  imports: [CommonModule, RatingModule, FormsModule],
   templateUrl: './game-list-component.component.html',
   styleUrl: './game-list-component.component.scss'
 })
 export class GameListComponentComponent {
+
+  cancel: boolean = false
+  value: number = 3
 
 }
