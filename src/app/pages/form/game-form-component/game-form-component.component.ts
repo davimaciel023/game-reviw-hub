d116b9eb26e685717f395cd1838ba2c22c39fdda
davimaciel077@gmail.com <davimaciel077@gmail.com>
2025-06-18 22:28:07 -0300

    configurando cadastro de games

--- src/app/pages/form/game-form-component/game-form-component.component.ts
+++ src/app/pages/form/game-form-component/game-form-component.component.ts
@@ -1,11 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-game-form-component',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './game-form-component.component.html',
   styleUrl: './game-form-component.component.scss'
 })
 export class GameFormComponentComponent {
 
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.form = this.fb.group({
+      title: ['', Validators.required],
+      genre: ['', Validators.required],
+      description: ['', Validators.required],
+      platform: ['', Validators.required],
+      imagem: ['', Validators.required]
+    })
+  }
+
+  ngOnInit(): void {}
 }
