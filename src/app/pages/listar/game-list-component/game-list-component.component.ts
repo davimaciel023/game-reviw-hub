764ac91727594d93743527616fc414d807b0befb
davimaciel077@gmail.com <davimaciel077@gmail.com>
2025-06-18 21:50:03 -0300

    commit

--- src/app/pages/listar/game-list-component/game-list-component.component.ts
+++ src/app/pages/listar/game-list-component/game-list-component.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import ratingMo
+
 
 @Component({
   selector: 'app-game-list-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './game-list-component.component.html',
   styleUrl: './game-list-component.component.scss'
 })
