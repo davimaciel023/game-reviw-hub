e6b53635e50ae571de4d9952b0a107ce1d948862
davimaciel077@gmail.com <davimaciel077@gmail.com>
2025-06-22 22:16:03 -0300

    commit

--- src/app/pages/detalhes/game-detail-component/game-detail-component.component.ts
+++ src/app/pages/detalhes/game-detail-component/game-detail-component.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Game } from '../../../interfaces/gameInterfaces.models';
+import { GameServiceService } from '../../../servicos/game-service.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-game-detail-component',
@@ -10,5 +12,18 @@ import { Game } from '../../../interfaces/gameInterfaces.models';
 export class GameDetailComponentComponent {
 
   game: Game[] = []
+  gameId: string = ''
+
+  constructor(
+    private service: GameServiceService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.gameId = String(this.route.snapshot.paramMap.get('id'))
+
+    this.service.pegarPorId(this.gameId).subscribe()
+  }
 
 }
