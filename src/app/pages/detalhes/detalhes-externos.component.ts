6e23fbd88a47bc0b0d3a67f3df3ae57c1e0f8a63
davimaciel077@gmail.com <davimaciel077@gmail.com>
2025-06-29 15:57:42 -0300

    commit

--- src/app/pages/detalhes/detalhes-externos.component.ts
+++ src/app/pages/detalhes/detalhes-externos.component.ts
@@ -24,15 +24,30 @@ export class DetalhesExternosComponent {
 
   ngOnInit(): void {
   const id = this.route.snapshot.paramMap.get('id');
-  console.log('ID recebido:', id); // <-- debug
-  this.http.get(`https://api.rawg.io/api/games/${id}?key=${this.API_KEY}`).subscribe({
-    next: (res: any) => {
-      this.game = res;
-    },
-    error: (err) => {
-      console.error('Erro ao carregar jogo externo:', err);
-      alert('Não foi possível carregar o jogo');
-    }
+
+  this.http.get(`https://api.rawg.io/api/games/${id}?key=${this.API_KEY}`).subscribe((res: any) => {
+    this.game = res;
+    this.generos = res.genres.map((g: any) => g.name).join(', ');
+    this.plataformas = res.platforms.map((p: any) => p.platform.name).join(', ');
+
+    // Tradução da descrição
+    this.traduzirDescricao(res.description_raw).subscribe((traduzido: any) => {
+      this.game.description = traduzido.translatedText;
+    });
+  }, err => {
+    console.error('Erro ao carregar o jogo:', err);
   });
 }
+
+
+  traduzirDescricao(texto: string) {
+    const body = {
+      q: texto,
+      source: 'en',
+      target: 'pt',
+      format: 'text'
+    };
+
+    return this.http.post('https://libretranslate.de/translate', body);
+  }
 }
