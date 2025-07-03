db23038fbe316405590d8963081f05138237ec84
davimaciel077@gmail.com <davimaciel077@gmail.com>
2025-07-02 21:27:01 -0300

    configurando tradução

--- src/app/pages/detalhes/detalhes-externos.component.ts
+++ src/app/pages/detalhes/detalhes-externos.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-detalhes-externos',
@@ -14,6 +14,7 @@ export class DetalhesExternosComponent {
   game: any;
   generos: string = '';
   plataformas: string = '';
+  descricaoTraduzida: string = '';
 
   private readonly API_KEY = '638c56342dc9447fa2df5184a9ecaf99';
 
@@ -23,30 +24,33 @@ export class DetalhesExternosComponent {
   ) {}
 
   ngOnInit(): void {
-  const id = this.route.snapshot.paramMap.get('id');
-
-  this.http.get(`https://api.rawg.io/api/games/${id}?key=${this.API_KEY}`).subscribe((res: any) => {
-    this.game = res;
-    this.generos = res.genres.map((g: any) => g.name).join(', ');
-    this.plataformas = res.platforms.map((p: any) => p.platform.name).join(', ');
-
-    this.traduzirDescricao(res.description_raw).subscribe((traduzido: any) => {
-      this.game.description = traduzido.translatedText;
+    const id = this.route.snapshot.paramMap.get('id');
+    this.http.get(`https://api.rawg.io/api/games/${id}?key=${this.API_KEY}`).subscribe((res: any) => {
+      this.game = res;
+      this.generos = res.genres.map((g: any) => g.name).join(', ');
+      this.plataformas = res.platforms.map((p: any) => p.platform.name).join(', ');
+      this.traduzirDescricao(res.description_raw);
     });
-  }, err => {
-    console.error('Erro ao carregar o jogo:', err);
-  });
-}
+  }
 
-
-  traduzirDescricao(texto: string) {
+  traduzirDescricao(textoOriginal: string) {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     const body = {
-      q: texto,
+      q: textoOriginal,
       source: 'en',
       target: 'pt',
       format: 'text'
     };
 
-    return this.http.post('https://libretranslate.de/translate', body);
+    this.http.post<any>('https://translate.astian.org/translate', body, { headers })
+      .subscribe({
+        next: (res) => {
+          this.descricaoTraduzida = res.translatedText;
+        },
+        error: (err) => {
+          console.error('Erro ao traduzir:', err);
+          this.descricaoTraduzida = '⚠️ Não foi possível traduzir a descrição.';
+        }
+      });
   }
 }
